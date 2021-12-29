4c1d9b9202188991206c82b82d766c81bbe5cea5
teo <teo@gmail.com>
2021-12-29 09:52:26 +0700

    modify file v29120952

---
+++ js/utils/post.js
@@ -0,0 +1,48 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { setTextContent, truncateText } from './common'
+dayjs.extend(relativeTime)
+
+export function createPostElement(post) {
+  if (!post) return
+
+  //   find and clone template
+  const postTemplate = document.getElementById('postTemplate')
+  if (!postTemplate) return
+
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true)
+  if (!liElement) return
+
+  setTextContent(liElement, '[data-id="title"]', post.title)
+  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100))
+  setTextContent(liElement, '[data-id="author"]', post.author)
+
+  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
+  if (thumbnailElement) {
+    thumbnailElement.src = post.imageUrl
+
+    thumbnailElement.addEventListener('error', () => {
+      thumbnailElement.src = 'https://via.placeholder.com/1768x400.png?text=thumbnail'
+    })
+  }
+
+  setTextContent(liElement, '[data-id="timeSpan"]', `- ${dayjs(post.createdAt).fromNow()}`)
+
+  return liElement
+}
+
+export function renderPostLists(postList) {
+  if (!Array.isArray(postList)) return
+
+  const ulElement = document.getElementById('postList')
+  if (!ulElement) return
+
+  // clear current ul
+  ulElement.textContent = ''
+
+  postList.forEach((post, index) => {
+    const liElement = createPostElement(post)
+
+    ulElement.appendChild(liElement)
+  })
+}
